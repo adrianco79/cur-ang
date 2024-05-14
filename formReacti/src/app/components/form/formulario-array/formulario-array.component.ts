4f30472b63b1a2248a4bc2f7b3f61e6891b2b477
adrianco79 <acorrea@unne.edu.ar>
2024-05-14 12:35:57 -0300

    formularios array
    formularios array

--- formReacti/src/app/components/form/formulario-array/formulario-array.component.ts
+++ formReacti/src/app/components/form/formulario-array/formulario-array.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, FormArray } from '@angular/forms'
 
 @Component({
   selector: 'app-formulario-array',
   templateUrl: './formulario-array.component.html',
   styleUrl: './formulario-array.component.css'
 })
-export class FormularioArrayComponent {
+export class FormularioArrayComponent implements OnInit {
+
+  miFormularioArray:FormGroup=new FormGroup({});
+
+   constructor(private fb:FormBuilder){}
+
+
+  //Metodo getter par obtener el FormArray de la lista de telefono
+get telefonosFormularios():FormArray{
+return this.miFormularioArray.get('telefonos') as FormArray
+}
+
+
+añadirTelefono(){
+  //creamos un grupo telefonos
+
+  const telefonoNuevo=this.fb.group({
+    prefijo:'',
+    numero:''
+  })
+
+  //Añadimos el grupo a la lista de telefonos
+
+  this.telefonosFormularios.push(telefonoNuevo);
+
+}
+
+
+// metodo para eliminar telefonos
+
+eliminarTelefono(index:number){
+  this.telefonosFormularios.removeAt(index);
+}
+
+
+  ngOnInit(): void {
+
+    this.miFormularioArray= this.fb.group({
+      nombre:'',
+      apellido:'',
+      telefonos:this.fb.array([])// inicializamos vacios la lista de telefonos
+    })
+
+  }
+
+
+
+
 
 }
